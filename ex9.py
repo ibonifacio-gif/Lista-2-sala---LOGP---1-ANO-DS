371516408a05c82ae0867db0492b065e69720631
ibonifacio-gif <i.bonifacio@aluno.ifsp.edu.br>
2025-10-19 22:49:45 -0300

    ex9.py

---
+++ ex9.py
@@ -0,0 +1,12 @@
+a = int(input("Digite o primeiro número:"))
+b = int(input("Digite o segundo número:"))
+
+if a%5==0:
+    print(a, "é divisivel por 5")
+elif a%4==0:
+    print(a, "é divísivel por 4")
+
+if b%5==0:
+    print(b, "é divisivel por 5")
+elif b%4==0:
+    print(b, "é divisivel por 5")
