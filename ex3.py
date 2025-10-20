dfae002aee0c4778bd1c47a8552480b472a55a48
ibonifacio-gif <i.bonifacio@aluno.ifsp.edu.br>
2025-10-19 22:46:44 -0300

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,11 @@
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+
+x = a-b
+y= b-a
+
+if a>b:
+    print("A diferença do maior pro menor é", x)
+
+else:
+    print("A diferença do maior pro menor é", y)
