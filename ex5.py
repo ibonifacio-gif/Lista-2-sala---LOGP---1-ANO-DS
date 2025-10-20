b47ad482cd428c64b40b6870e742282cc9865ba6
ibonifacio-gif <i.bonifacio@aluno.ifsp.edu.br>
2025-10-19 22:47:46 -0300

    ex5.py

---
+++ ex5.py
@@ -0,0 +1,7 @@
+a = int(input("Digite o primeiro número:"))
+b = int(input("Digite o segundo número:"))
+c = int(input("Digite o terceiro número:"))
+
+na_ordem = sorted([a,b,c])
+
+print("Os números em ordem crescente:", na_ordem)
